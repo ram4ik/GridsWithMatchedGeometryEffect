844fca078fb78373a460f1ca03b15b77f4b64e33
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-06-25 14:20:09 +0300

    Initial Commit

---
+++ GridsWithMatchedGeometryEffect/GridsWithMatchedGeometryEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridsWithMatchedGeometryEffectApp.swift
+//  GridsWithMatchedGeometryEffect
+//
+//  Created by ramil on 25.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct GridsWithMatchedGeometryEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
